71d1fbe4c679a5a3610d6d4c111f9e0b2a9ef107
fabianromerolu <fabianromerolu@gmail.com>
2024-07-07 20:03:49 -0500

    RETOMANDO

--- src/hooks/useApp.js
+++ src/hooks/useApp.js
@@ -4,15 +4,6 @@ const useApp = () => {
     const [characters, setCharacters] = useState([]);
    
     const onSearch = async (id) => {
-       // EST QUE ESTA COMENTADO ES POR PROMESA, RECUERDA QUE
-       // DEBES QUITAR EL ASYNC DE ARRIBA DE ESTA LINEA Y DESCOMENTAR TOOD Y QUITAR LO DE AWAIT
-       //setCharacters([...characters]);
-       //fetch(URL + id)
-       //.then((response) => response.json())
-       //.then((data) => {
-          //setCharacters([...characters, data]);
-       //});
-       //.error <= para manejar los errores con promesas
         try {
             //async await
             const response = await fetch (URL + id);
